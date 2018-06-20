46fb00251d72b3bf468f3ff95e509cda333f704e
baramnamu <32696857+baramnamu@users.noreply.github.com>
2018-06-20 18:34:23 +0900

    modified by security audit report

--- TokenERC20.sol
+++ TokenERC20.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.21;
 
+import "./SafeMath.sol";
+
 contract Ownable {
     address internal owner;
 
@@ -64,22 +66,24 @@ interface tokenRecipient { function receiveApproval(address _from, uint256 _valu
  * version 12
  */
 contract TokenERC20 is Pausable {
+    using SafeMath for uint256;
+    
     // Public variables of the token
     string public name;
     string public symbol;
     uint8 public decimals = 18;    // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
 
-    uint256 public sellPrice;
-    uint256 public buyPrice;
-    
     /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowed;
     mapping (address => bool) public frozenAccount;
 
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    // This notifies clients about the allowance of balance
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     // This notifies clients about the amount burnt
     event Burn(address indexed from, uint256 value);
@@ -87,7 +91,7 @@ contract TokenERC20 is Pausable {
     // This notifies clients about the amount resupplied
     event Resupply(address indexed from, uint256 value);
     
-    /* This generates a public event on the blockchain that will notify clients */
+    // This notifies clients about the freezing address
     event FrozenFunds(address target, bool frozen);
 
     /**
@@ -95,14 +99,12 @@ contract TokenERC20 is Pausable {
      *
      * Initializes contract with initial supply tokens to the creator of the contract
      */
-    constructor(string tokenName, string tokenSymbol, uint256 initialSupply, uint8 decimalUnits, uint256 newSellPrice, uint256 newBuyPrice) public {
+    constructor(string tokenName, string tokenSymbol, uint256 initialSupply, uint8 decimalUnits) public {
         name = tokenName;                                       // Set the name for display purposes
         symbol = tokenSymbol;                                   // Set the symbol for display purposes
         if (decimalUnits > 0) decimals = decimalUnits;
         totalSupply = convertToDecimalUnits(initialSupply);     // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply;                    // Give the creator all initial tokens
-        sellPrice = newSellPrice;
-        buyPrice = newBuyPrice;
+        balances[msg.sender] = totalSupply;                     // Give the creator all initial tokens
     }
     
     /**
@@ -111,7 +113,7 @@ contract TokenERC20 is Pausable {
      * @param _value Tokens units without decimal units 
      */
     function convertToDecimalUnits(uint256 _value) internal view returns (uint256 value) {
-        value = _value * 10 ** uint256(decimals);
+        value = _value.mul(10 ** uint256(decimals));
         return value;
     }
     
@@ -122,28 +124,40 @@ contract TokenERC20 is Pausable {
      * 
      * @param _account Account address to query tokens balance
      */
-    function getBalance(address _account) public view returns (uint256 balance) {
-        balance = balanceOf[_account];
+    function balanceOf(address _account) external view returns (uint256 balance) {
+        balance = balances[_account];
         return balance;
+    }
+    
+    /**
+     * Get allowed tokens balance 
+     * 
+     * @notice Query tokens balance allowed to _spender
+     * 
+     * @param _owner Owner address to query tokens balance
+     * @param _spender The address allowed tokens balance
+     */
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining) {
+        remaining = allowed[_owner][_spender];
+        return remaining;
     }
     
     /**
      * Internal transfer, only can be called by this contract
      */
     function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != 0x0);                                        // Prevent transfer to 0x0 address. Use burn() instead
-        require(balanceOf[_from] >= _value);                        // Check if the sender has enough
-        require(balanceOf[_to] + _value >= balanceOf[_to]);         // Check for overflows
-        require(!frozenAccount[_from]);                             // Check if sender is frozen
-        require(!frozenAccount[_to]);                               // Check if recipient is frozen
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];  // Save this for an assertion in the future
+        require(_to != 0x0);                                            // Prevent transfer to 0x0 address. Use burn() instead
+        require(balances[_from] >= _value);                             // Check if the sender has enough
+        require(!frozenAccount[_from]);                                 // Check if sender is frozen
+        require(!frozenAccount[_to]);                                   // Check if recipient is frozen
+        uint previousBalances = balances[_from].add(balances[_to]);     // Save this for an assertion in the future
         
-        balanceOf[_from] -= _value;                                 // Subtract from the sender
-        balanceOf[_to] += _value;                                   // Add the same to the recipient
+        balances[_from] = balances[_from].sub(_value);                  // Subtract from the sender
+        balances[_to] = balances[_to].add(_value);                      // Add the same to the recipient
         emit Transfer(_from, _to, _value);
         
         // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+        assert(balances[_from] + balances[_to] == previousBalances);
     }
     
     /**
@@ -154,8 +168,10 @@ contract TokenERC20 is Pausable {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transfer(address _to, uint256 _value) public noReentrancy {
+    function transfer(address _to, uint256 _value) public noReentrancy returns (bool success) {
         _transfer(msg.sender, _to, _value);
+        success = true;
+        return success;
     }
 
     /**
@@ -168,8 +184,8 @@ contract TokenERC20 is Pausable {
      * @param _value the amount to send
      */
     function transferFrom(address _from, address _to, uint256 _value) public noReentrancy returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
+        require(_value <= allowed[_from][msg.sender]);     // Check allowance
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
         _transfer(_from, _to, _value);
         return true;
     }
@@ -183,7 +199,8 @@ contract TokenERC20 is Pausable {
      * @param _value the max amount they can spend
      */
     function approve(address _spender, uint256 _value) public noReentrancy returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
@@ -212,9 +229,9 @@ contract TokenERC20 is Pausable {
      * @param _value the amount of money to burn
      */
     function burn(uint256 _value) onlyOwner public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
-        totalSupply -= _value;                      // Updates totalSupply
+        require(balances[msg.sender] >= _value);                            // Check if the sender has enough
+        balances[msg.sender] = balances[msg.sender].sub(_value);            // Subtract from the sender
+        totalSupply = totalSupply.sub(_value);                              // Updates totalSupply
         emit Burn(msg.sender, _value);
         return true;
     }
@@ -228,11 +245,11 @@ contract TokenERC20 is Pausable {
      * @param _value the amount of money to burn
      */
     function burnFrom(address _from, uint256 _value) onlyOwner public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
+        require(balances[_from] >= _value);                                         // Check if the targeted balance is enough
+        require(allowed[_from][msg.sender] >= _value);                              // Check allowance
+        balances[_from] = balances[_from].sub(_value);                              // Subtract from the targeted balance
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);        // Subtract from the sender's allowance
+        totalSupply = totalSupply.sub(_value);                                      // Update totalSupply
         emit Burn(_from, _value);
         return true;
     }
@@ -246,9 +263,8 @@ contract TokenERC20 is Pausable {
      * @param mintedAmount the amount of tokens it will receive
      */
     function mintToken(address target, uint256 mintedAmount) onlyOwner public {
-        require(totalSupply + mintedAmount >= totalSupply);                 // Check for overflows
-        balanceOf[target] += mintedAmount;
-        totalSupply += mintedAmount;
+        balances[target] = balances[target].add(mintedAmount);
+        totalSupply = totalSupply.add(mintedAmount);
         emit Transfer(0, this, mintedAmount);
         emit Transfer(this, target, mintedAmount);
     }
@@ -266,47 +282,12 @@ contract TokenERC20 is Pausable {
         frozenAccount[target] = false;
         emit FrozenFunds(target, false);
     }
-
-    /**
-     * Set sellPrice and buyPrice
-     * 
-     * @notice Allow users to buy tokens for `newBuyPrice` eth and sell tokens for `newSellPrice` eth
-     * 
-     * @param newSellPrice Price the users can sell to the contract
-     * @param newBuyPrice Price users can buy from the contract
-     */
-    function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner public {
-        sellPrice = newSellPrice;
-        buyPrice = newBuyPrice;
-    }
-    
-    /**
-     * @notice Buy tokens from contract by sending ether
-     */
-    function buy() payable public noReentrancy returns (uint256 amount) {
-        amount = msg.value * buyPrice;                                          // calculates the amount
-        _transfer(this, msg.sender, amount);    // makes the transfers
-        return amount;
-    }
-    
-    /**
-     * @notice Sell `amount` tokens to contract
-     * 
-     * @param amount amount of tokens to be sold
-     */
-    function sell(uint256 amount) public noReentrancy returns (uint256 revenue) {
-        revenue = amount / sellPrice;
-        require(address(this).balance >= revenue);                              // checks if the contract has enough ether to buy
-        _transfer(msg.sender, this, amount);    // makes the transfers
-        // sends ether to the seller. It's important to do this last to avoid recursion attacks
-        msg.sender.transfer(revenue);
-        return revenue;
-    }
     
     /**
      * Destroy this contract
      *
      * @notice Remove this contract from the system irreversibly and send remain funds to owner account
+     * @notice 정식 배포시 삭제예정
      */
     function destroy() external onlyOwner {
         selfdestruct(owner);
@@ -316,6 +297,7 @@ contract TokenERC20 is Pausable {
      * Destroy this contract
      *
      * @notice Remove this contract from the system irreversibly and send remain funds to _recipient account
+     * @notice 정식 배포시 삭제예정
      * 
      * @param _recipient Address to receive the funds
      */
